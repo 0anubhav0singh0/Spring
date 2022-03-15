234bc022e6165d2cb1fe0a106ba8572d0362b14f
0anubhav0singh0 <101598361+0anubhav0singh0@users.noreply.github.com>
2022-03-15 23:53:18 +0530

    Update PatientDAO.java

--- Introduction to Spring Boot/POST and Get Using @RequestMapping/src/main/java/com/springboot/dao/PatientDAO.java
+++ Introduction to Spring Boot/POST and Get Using @RequestMapping/src/main/java/com/springboot/dao/PatientDAO.java
@@ -1,13 +1,15 @@
 package com.springboot.dao;
 
 
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
 import java.util.List;
 
 
 import com.springboot.domain.Patient;
 
-//Fill the code
+@Component
 public class PatientDAO {
 	
 	static List<Patient> patientList = new ArrayList<>(); 
@@ -18,12 +20,23 @@ public class PatientDAO {
 			patientList.add(new Patient(3L, "Kevin", "9823641234", "kevin@gmail.com", "01-04-2000"));
 	}
 	
+	/*
+	This method accepts a patient object and adds it to the 
+	patient list and returns the new list
+	*/
 	public List<Patient> createPatient(Patient p){
 		//Fill the code
+		patientList.add(p);
+		return PatientDAO.patientList;
 	}
 	
+	/*
+	This method is used to return patient list and the patient 
+	details are already preloaded in the template code.
+	*/
 	public List<Patient> getPatients(){
 		//Fill the code
+		return PatientDAO.patientList;
 	}
 	
 }
